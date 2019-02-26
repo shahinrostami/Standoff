b282ec8e8d4c72f0cc9e762f45d3828c860b4ecf
Dr. Shahin Rostami <srostami@bournemoouth.ac.uk>
2019-02-26 14:13:46 +0000

    initial commit

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	public PlayerScript Player;
+	float movementSpeed = 0.25f;
+	int step = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!LeanTween.isTweening (Player.gameObject)) {
+			if (Input.GetKeyDown (KeyCode.W) ||
+			   Input.GetKeyDown (KeyCode.A) ||
+			   Input.GetKeyDown (KeyCode.S) ||
+			   Input.GetKeyDown (KeyCode.D)) {
+				Vector3 diff = (transform.position - Player.transform.position);
+
+				if (Mathf.Abs (diff.x) + Mathf.Abs (diff.z) <= 1) {
+					step = 2;
+				} else {
+					step = 1;
+				}
+
+
+					LeanTween.moveX (gameObject, transform.position.x - (step * (diff.x / Mathf.Abs(diff.x))), movementSpeed);	
+				
+					LeanTween.moveZ (gameObject, transform.position.z - (step * (diff.z / Mathf.Abs(diff.z))), movementSpeed);	
+
+
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		// do something when caught
+		Destroy(col.gameObject);
+	}
+
+
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	float movementSpeed = 0.25f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!LeanTween.isTweening(gameObject)) {
+			if (Input.GetKeyDown (KeyCode.W)) {
+				LeanTween.moveZ (gameObject, transform.position.z + 1f, movementSpeed);	
+			}
+			else if (Input.GetKeyDown (KeyCode.S)) {
+				LeanTween.moveZ (gameObject, transform.position.z - 1f, movementSpeed);	
+			}
+			else if (Input.GetKeyDown (KeyCode.A)) {
+				LeanTween.moveX (gameObject, transform.position.x - 1f, movementSpeed);	
+			}
+			else 	if (Input.GetKeyDown (KeyCode.D)) {
+				LeanTween.moveX (gameObject, transform.position.x + 1f, movementSpeed);	
+			}
+		}
+	}
+}
